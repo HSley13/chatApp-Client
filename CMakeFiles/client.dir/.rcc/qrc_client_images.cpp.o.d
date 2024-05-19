4003d157ad5d07c9647af6fbdebde67257b7f2ba
Sley HORTES <sleyhortes13@gmail.com>
2024-05-19 15:44:21 +0800

    Changing the server's ip address

--- CMakeFiles/client.dir/.rcc/qrc_client_images.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/client.dir/.rcc/qrc_client_images.cpp.o: \
-  /Users/test/Documents/client_app/.rcc/qrc_client_images.cpp
--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_client_FILE /Users/test/Documents/client_app/build/client.app/Contents/MacOS/client)
